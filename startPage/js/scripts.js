3b29caac9067e8d1adba09f6f55766d2bc7bf325
domanskyi <bruce3773@gmail.com>
2018-08-17 01:41:26 +0300

    fixed bug with active element

--- startPage/js/scripts.js
+++ startPage/js/scripts.js
@@ -1,29 +1,34 @@
 //PZDC GOVNOKOD
 document.onkeypress = function (e) {
-    console.log(e);
+    $active = document.activeElement;
 
-    switch (e.key) {
-        case 't':
-        case 'е':
-            redirect('https://twitter.com/');
-            break;
-        case 'i':
-        case 'ш':
-            redirect('https://www.instagram.com/?hl=en');
-            break;
-        case 'g':
-        case 'п':
-            redirect('https://github.com/');
-            break;
-        case 'y':
-        case 'н':
-            redirect('https://www.youtube.com/');
-            break;
-        case 's':
-        case 'ы':
-        case 'і':
-            focusSearch();
-    }   
+    if ($active.id != 'search_field') {
+
+        switch (e.key) {
+            case 't':
+            case 'е':
+                redirect('https://twitter.com/');
+                break;
+            case 'i':
+            case 'ш':
+                redirect('https://www.instagram.com/?hl=en');
+                break;
+            case 'g':
+            case 'п':
+                redirect('https://github.com/');
+                break;
+            case 'y':
+            case 'н':
+                redirect('https://www.youtube.com/');
+                break;
+            case 's':
+            case 'ы':
+            case 'і':
+                focusSearch();
+        } 
+        
+    }
+
 }
 
 //find better solution to this^^^
